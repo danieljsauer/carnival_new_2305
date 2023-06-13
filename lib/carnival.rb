5e78749044ed667ae89d2a89b706b8937ce86f55
Daniel Sauer <danieljsauer92@gmail.com>
2023-06-13 11:04:25 -0600

    feat: create most profitable ride method

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -12,4 +12,20 @@ class Carnival
     @rides << ride
   end 
 
+  def most_profitable
+    return "No rides at carnival :(" if @rides.empty?
+  
+    most_profitable_ride = @rides[0]
+    max_revenue = 0
+  
+    @rides.each do |ride|
+      if ride.total_revenue > max_revenue
+        max_revenue = ride.total_revenue
+        most_profitable_ride = ride
+      end
+    end
+  
+    most_profitable_ride.name
+  end
+  
 end 
