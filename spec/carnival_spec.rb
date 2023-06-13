0ce8d880cabe481c9782b8c2885d97471be138b0
Daniel Sauer <danieljsauer92@gmail.com>
2023-06-13 11:07:40 -0600

    test: include expect for default total revenue value of 0

--- spec/carnival_spec.rb
+++ spec/carnival_spec.rb
@@ -51,9 +51,11 @@ describe Carnival do
       carnival = Carnival.new("Twistys", 14)
       ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
       ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
-      
+
       visitor1 = Visitor.new('Bruce', 54, 10)
       visitor1.add_preference(:gentle)
+
+      expect(carnival.total_revenue).to eq(0)
 
       ride1.board_rider(visitor1)
       ride2.board_rider(visitor1)
