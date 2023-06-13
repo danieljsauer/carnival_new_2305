b1c48b66ff22a7325f8686ada470ee2a3f6491fc
Daniel Sauer <danieljsauer92@gmail.com>
2023-06-13 09:43:06 -0600

    test: add test for board rider method

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -27,7 +27,17 @@ describe Ride do
   end 
 
   describe "instance methods" do 
-    xit "can board riders if they are "
+    it "can add riders into rider log if they are tall enough and have enough cash" do 
+      ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+      visitor1 = Visitor.new('Bruce', 54, '$10')
+      visitor2 = Visitor.new('Tucker', 20, '$5')
+      visitor3 = Visitor.new('Penny', 64, '$0')
+      expect(ride1.board_rider(visitor2)).to eq("#{visitor2.name} is not tall enough!")
+      expect(ride1.board_rider(visitor3)).to eq("#{visitor3.name} cannot afford it!")
+
+      ride1.board_rider(visitor1)
+      expect(ride1.rider_log).to include(visitor1)
+    end 
   end 
 
 end 
